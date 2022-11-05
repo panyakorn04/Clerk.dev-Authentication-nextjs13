fcda90856026a53f39ad1be9f06c99d4032064bd
Eee.dev <82938183+panyakorn04@users.noreply.github.com>
2022-11-05 20:01:23 +0700

    Update page.tsx

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from '@clerk/nextjs/app-beta';
 
 export default function Page() {
-    return <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />;
+    return <SignUp />;
 }
